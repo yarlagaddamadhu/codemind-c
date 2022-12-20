26a8bf139796f30e1d9dc46201738babe100a9b8
yarlagaddamadhu <119730607+yarlagaddamadhu@users.noreply.github.com>
2022-12-20 20:04:41 +0530

    #include<stdio.h> int main() {     int x,i;     scanf("%d",&x);     for(i=1;i<=x;i++)     {         int n;         scanf("%d",&n);         int rev=0,t=n;         while(n!=0)         {             rev=rev*10+n%10;             n=n/10;         }         if(t==rev)         {             printf("True ");     }     else     {         printf("False ");     }     } }

---
+++ Palindrome_Number.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int x,i;
+    scanf("%d",&x);
+    for(i=1;i<=x;i++)
+    {
+        int n;
+        scanf("%d",&n);
+        int rev=0,t=n;
+        while(n!=0)
+        {
+            rev=rev*10+n%10;
+            n=n/10;
+        }
+        if(t==rev)
+        {
+            printf("True
+");
+    }
+    else
+    {
+        printf("False
+");
+    }
+    }
+}
